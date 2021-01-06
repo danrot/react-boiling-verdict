b35caff6506645854b19ba391416e58c52988279
Daniel Rotter <daniel.rotter@gmail.com>
2021-01-06 16:34:59 +0100

    Add button with preset temperature

--- src/TemperatureInput.js
+++ src/TemperatureInput.js
@@ -18,6 +18,11 @@ export default function TemperatureInput({onChange, value, onUnitChange, unit}) 
         onUnitChange(newUnit);
     }
 
+    function setTemperature(temperature, unit) {
+        onChange(temperature);
+        onUnitChange(unit);
+    }
+
     return (
         <fieldset>
             <input type="number" step={0.1} onChange={(event) => onChange(event.target.value)} value={value} />
@@ -25,6 +30,7 @@ export default function TemperatureInput({onChange, value, onUnitChange, unit}) 
                 <option value="c">°C</option>
                 <option value="f">°F</option>
             </select>
+            <button onClick={() => setTemperature(37, 'c')}>Set 37°C</button>
         </fieldset>
     );
 }
