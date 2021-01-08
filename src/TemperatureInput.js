37265a815d9b511a81c58bb9abbab4182c43d9c6
Daniel Rotter <daniel.rotter@gmail.com>
2021-01-08 07:52:44 +0100

    Do not convert temperature when unit is switched

--- src/TemperatureInput.js
+++ src/TemperatureInput.js
@@ -2,22 +2,6 @@ import React from 'react';
 import {convertCelsiusToFahrenheit, convertFahrenheitToCelsius} from './temperature';
 
 export default function TemperatureInput({onChange, value, onUnitChange, unit}) {
-    function handleUnitChange(event) {
-        const newUnit = event.target.value;
-
-        if (newUnit === unit) {
-            return;
-        }
-
-        if (unit === 'c' && newUnit === 'f') {
-            onChange(convertCelsiusToFahrenheit(value));
-        } else if(unit === 'f' && newUnit === 'c') {
-            onChange(convertFahrenheitToCelsius(value));
-        }
-
-        onUnitChange(newUnit);
-    }
-
     function setTemperature(temperature, unit) {
         onChange(temperature);
         onUnitChange(unit);
@@ -26,7 +10,7 @@ export default function TemperatureInput({onChange, value, onUnitChange, unit}) 
     return (
         <fieldset>
             <input type="number" step={0.1} onChange={(event) => onChange(event.target.value)} value={value} />
-            <select onChange={handleUnitChange} value={unit}>
+            <select onChange={(event) => onUnitChange(event.target.value)} value={unit}>
                 <option value="c">°C</option>
                 <option value="f">°F</option>
             </select>
--- src/temperature.js
+++ src/temperature.js
@@ -1,7 +1,3 @@
-export function convertCelsiusToFahrenheit(celsius) {
-    return celsius * 9 / 5 + 32;
-}
-
 export function convertFahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
 }
